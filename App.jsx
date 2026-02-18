8ec5fc331ba446f953489ca4cb12d90bbc3cb232
gabrielasoaresreis-boop <gabriela.soaresreis@gmail.com>
2026-02-18 16:07:44 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import ScrollToTop from '@/components/ScrollToTop';
-import LandingPage from '@/pages/LandingPage';
-import Checkout from '@/pages/Checkout';
-import ThankYou from '@/pages/ThankYou';
+import ScrollToTop from './components/ScrollToTop';
+import LandingPage from './pages/LandingPage';
+import Checkout from './pages/Checkout';
+import ThankYou from './pages/ThankYou';
 
 function App() {
   return (
